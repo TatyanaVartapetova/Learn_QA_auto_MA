1e041c4eeb5813e543a027d42ed737aebeefe813
Tatyana <94926482+TatyanaVartapetova@users.noreply.github.com>
2024-01-21 18:32:37 +0300

    Second Test

--- JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
@@ -4,11 +4,8 @@ import org.junit.Test;
 public class MainClassTest extends MainClass
 {
     @Test
-    public void testGetLocalNumber()
-    {
-        int actual =getLocalNumber();
-        int expected =14;
-        Assert.assertTrue("number is not 14", actual==expected);
-
+public void testGetClassNumber(){
+        int actual = getClassNumber();
+        Assert.assertTrue("number <= 45",actual>45);
     }
 }
